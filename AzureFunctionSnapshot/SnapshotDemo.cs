7c7b7d0bcdb3fd8174a2995ad8c321c99e8b451a
Tim Rose <flowor@users.noreply.github.com>
2024-10-07 15:35:40 -0400

    Add project files.

---
+++ AzureFunctionSnapshot/SnapshotDemo.cs
@@ -0,0 +1,37 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Playwright;
+using System.Net;
+
+namespace AzureFunctionSnapshot
+{
+    public class SnapshotDemo
+    {
+        private readonly ILogger _logger;
+
+        public SnapshotDemo(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<SnapshotDemo>();
+        }
+
+        [Function(nameof(SnapshotDemo))]
+        public async Task<HttpResponseData> TakeSnapshotAsync([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("Processing snapshot request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "image/png");
+
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new() { Headless = true });
+            var page = await browser.NewPageAsync();
+
+            await page.GotoAsync("https://example.com");
+
+            response.WriteBytes(await page.ScreenshotAsync());
+
+            return response;
+        }
+    }
+}
