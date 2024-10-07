04db4209733d4b226ec91dc198d756fef0947f58
Tim Rose <flowor@users.noreply.github.com>
2024-10-07 15:59:47 -0400

    Add install command

--- AzureFunctionSnapshot/Program.cs
+++ AzureFunctionSnapshot/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.Extensions.Hosting;
 
+Environment.SetEnvironmentVariable("PLAYWRIGHT_BROWSERS_PATH", Environment.GetEnvironmentVariable("HOME_EXPANDED"));
+Microsoft.Playwright.Program.Main(new[] { "install", "chromium", "--with-deps" });
+
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .Build();
